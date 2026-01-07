1a9a25ab2bcb3c4241144335fd67c5b99e2f063d
radoslavhrobar <hrobar1@stud.uniza.sk>
2026-01-08 00:11:35 +0100

    upraveny frontend pre pridavanie recenzii + spravene pridanie recenzii

--- vaiicko/App/Controllers/ReviewController.php
+++ vaiicko/App/Controllers/ReviewController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use App\Models\Review;
+use Exception;
 use Framework\Core\BaseController;
 use Framework\Http\Request;
 use Framework\Http\Responses\Response;
@@ -11,5 +13,27 @@ class ReviewController extends BaseController
     public function index(Request $request): Response
     {
         return $this->html();
+    }
+
+    public function add(Request $request): Response
+    {
+        $user = $this->app->getAuth()->getUser();
+        if (!$user) {
+            throw new Exception("Používateľ nenájdený.");
+        }
+        $data = $request->post();
+        $dataGet = $request->get();
+        if (!isset($data['title'], $data['body'], $data['rating'], $dataGet['workId'])) {
+            throw new Exception("Nedostatočné údaje o recenzii.");
+        }
+        $review = new Review();
+        $review->setTitle($data['title']);
+        $review->setBody($data['body']);
+        $review->setRating((int)$data['rating']);
+        $review->setWorkId((int)$dataGet['workId']);
+        $review->setUserId($user->getId());
+        $review->setCreatedAt(date('Y-m-d H:i:s'));
+        $review->save();
+        return $this->redirect($this->url("work.ownPage", ['id' => $dataGet['workId']]));
     }
 }
--- vaiicko/App/Views/Work/ownPage.view.php
+++ vaiicko/App/Views/Work/ownPage.view.php
@@ -101,21 +101,28 @@
         <div class="card mt-4">
             <div class="card-body">
                 <h5 class="fw-bold mb-3">Pridať recenziu</h5>
-                <form>
-                    <div class="mb-2">
-                            <textarea class="form-control"
-                                      rows="3"
-                                      placeholder="Napíš svoju recenziu..."></textarea>
+                <form  id="review" action="<?= $link->url("review.add", ['workId' => $work->getId()]) ?>" method="post">
+                    <div class="mb-3">
+                        <input type="text" class="form-control" name="title" placeholder="Názov recenzie">
                     </div>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <select class="form-select w-auto">
-                            <option>10</option>
-                            <option>9</option>
-                            <option>8</option>
-                            <option>7</option>
-                            <option>6</option>
-                        </select>
-                        <button class="btn btn-primary">
+                    <div class="mb-3">
+                        <textarea class="form-control" rows="3" name="body" placeholder="Napíš svoju recenziu..."></textarea>
+                    </div>
+                    <span>Hodnotenie:</span>
+                        <div class="d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center gap-2">
+                        <div class="star-rating">
+                            <input type="radio" id="star5" name="rating" value="5">
+                            <label for="star5">★★★★★</label>
+                            <input type="radio" id="star4" name="rating" value="4">
+                            <label for="star4">★★★★</label>
+                            <input type="radio" id="star3" name="rating" value="3">
+                            <label for="star3">★★★</label>
+                            <input type="radio" id="star2" name="rating" value="2">
+                            <label for="star2">★★</label>
+                            <input type="radio" id="star1" name="rating" value="1">
+                            <label for="star1">★</label>
+                        </div>
+                        <button type="submit" class="btn-brown">
                             Odoslať
                         </button>
                     </div>
